8f15eb3bc30e27c41c588edd8d8d09e0616e182b
Lee Evitts <evitts@triumf.ca>
2016-03-18 18:00:22 -0700

    FIX: Sometimes B() values are in exponent format

--- extract.py
+++ extract.py
@@ -4,7 +4,7 @@ import math
 def gReturnProton(fName):
   return {
     'NI': 28,
-  }.get(fName, "???")
+  }.get(fName, "000")
   
 def gCheckNumber(fChar):
   try:
@@ -13,14 +13,22 @@ def gCheckNumber(fChar):
   except ValueError:
     return False
     
-def gMatchError(fValue, fError):
+def gMatchError(fValue, fUpper, fLower):
+  if "E" in fValue:                                   # Sometimes B() values are in exponent format
+    fFront, fExponent = fValue.split("E")
+    if len(fUpper) > 1 or len(fLower) > 1:
+      sDiv = len(str(max(int(fUpper), int(fLower))))  # Biggest value
+      fUpper = str(float(fUpper) / 10**(sDiv-1)) 
+      fLower = str(float(fLower) / 10**(sDiv-1)) 
+    return (fUpper + "E" + fExponent, fLower + "E" + fExponent)
   try:
     fMain, fFraction = fValue.split('.')
     fPrecision = len(fFraction)
-    fError = float(fError) / 10**fPrecision
-    return str(fError)
+    fUpper = float(fUpper) / 10**fPrecision
+    fLower = float(fLower) / 10**fPrecision
+    return (str(fUpper), str(fLower))
   except ValueError:
-    return fError
+    return (fUpper, fLower)
     
 def gReturnB(fString):
   # fString is in the format of e.g. E2=0.49 +13-18, this code strips out the value from
@@ -31,11 +39,13 @@ def gReturnB(fString):
     sVector.append(sValue)
     if sError.strip()[0] == "+":
       sMatch = re.search(r"[^a-zA-Z](-)[^a-zA-Z]", sError)
-      sVector.append(gMatchError(sValue, sError[1:sMatch.start(1)]))
-      sVector.append(gMatchError(sValue, sError[sMatch.start(1)+1:]))
+      sUpper, sLower = gMatchError(sValue, sError[1:sMatch.start(1)], sError[sMatch.start(1)+1:])
+      sVector.append(sUpper)
+      sVector.append(sLower)
     else:
-      sVector.append(gMatchError(sValue, sError))
-      sVector.append(gMatchError(sValue, sError))
+      sUpper, sLower = gMatchError(sValue, sError, sError)
+      sVector.append(sUpper)
+      sVector.append(sLower)
   elif fString[2] == "<":
     sVector.append(fString[3:])
     sVector.append("0")
@@ -58,24 +68,24 @@ fLvlLifetime = []
 
 # Open the file, get the mass and proton number  
 fInput = open('data/60_28.dat','r')
-fOutput = open('collate.dat', 'w') # TODO Change to append after testing
 nucMass = int(fInput.read(3))
 nucName = fInput.read(2)
 nucProton = gReturnProton(nucName)
 
 # Read through each line in the file, firstly looking for energy levels
-for fLine in fInput:
-  if len(fLvlEnergy) == 100:               # Limit of the first x energy levels
-    break
-  if fLine[6] == "c" or fLine[5] == "X":
-    continue                              # Skip comments and cross-references
-  sTrsBM1 = []
-  sTrsBE2 = []
-  sTrsMixing = []
-  sTrsEnergy = 0.0
-  sTrsID = "1"
+sTrsBM1 = []
+sTrsBE2 = []
+sTrsMixing = []
+sTrsEnergy = 0.0
+sTrsID = "1"
 
-  if fLine[5:8] == "  L":                 # Energy level
+for fLine in fInput:
+  if len(fLvlEnergy) == 8:               # Limit of the first x energy levels
+    break
+  if fLine[5:8] != "  L" and fLine[5:8] != "  G" and fLine[5:8] != "B G":
+    continue                              # Skip everything that isn't energy level, transition, transition details
+
+  if fLine[5:8] == "  L":                 # Energy level 
     fLvlEnergy.append(float(fLine[9:18])) # Energy, omitting error (error will be in gamma)
     sSpin = fLine[21:25].strip()
 
@@ -117,8 +127,9 @@ for fLine in fInput:
       if fLine[49] == "+":                # Error (+/-)
         sSubstring = fLine[49:60].strip()
         sMatch = re.search(r"[^a-zA-Z](-)[^a-zA-Z]", sSubstring)
-        sList.append(gMatchError(str(sLifeString[:-2].strip()), str(sSubstring[1:sMatch.start(1)])))
-        sList.append(gMatchError(str(sLifeString[:-2].strip()), str(sSubstring[sMatch.start(1)+1:])))
+        sUpper, sLower = gMatchError(str(sLifeString[:-2].strip()), str(sSubstring[1:sMatch.start(1)]), str(sSubstring[sMatch.start(1)+1:]))
+        sList.append(sUpper)
+        sList.append(sLower)
       elif fLine[49] == "G":              # Error (GT)
         sList.append("0")                 # .. 0 error
         sList.append("0")
@@ -126,16 +137,33 @@ for fLine in fInput:
         sList.append("0")
         sList.append(str(sLifeString[:-2].strip()))
       else:
-        sList.append(gMatchError(str(sLifeString[:-2].strip()), str(fLine[49:60].strip())))
-        sList.append(gMatchError(str(sLifeString[:-2].strip()), str(fLine[49:60].strip())))
+        sUpper, sLower = gMatchError(str(sLifeString[:-2].strip()), str(fLine[49:60].strip()), str(fLine[49:60].strip()))
+        sList.append(sUpper)
+        sList.append(sLower)
       fLvlLifetime.append(sList)
   
   elif fLine[5:8] == "  G":                 # Transition
+  
+    # Encountering a new transition, write to file the previous one
+    print sTrsID, sTrsEnergy
+    if sTrsBE2:
+      print "B(E2) =", sTrsBE2[0], sTrsBE2[1], sTrsBE2[2]
+    if sTrsBM1:
+      print "B(M1) =", sTrsBM1[0], sTrsBM1[1], sTrsBM1[2]
+    if sTrsMixing:
+      print "B(E2) =", sTrsMixing[0], sTrsMixing[1], sTrsMixing[2]
+    # Reset the details
+    sTrsBM1 = []
+    sTrsBE2 = []
+    sTrsMixing = []
+    sTrsEnergy = 0.0
+    sTrsID = "1"
+     
     sTrsEnergy = float(fLine[9:18].strip())
     sFinEnergy = min(fLvlEnergy, key=lambda x:abs(x-(fLvlEnergy[-1] - sTrsEnergy))) # Find final state
     sIndex = fLvlEnergy.index(sFinEnergy)
     # Create transition ID
-    sTrsID += fLvlSpin[-1] + fLvlParity[-1] + fLvlCount[-1] + fLvlSpin[sIndex] + fLvlParity[sIndex] + fLvlCount[sIndex]
+    sTrsID = "1" + fLvlSpin[-1] + fLvlParity[-1] + fLvlCount[-1] + fLvlSpin[sIndex] + fLvlParity[sIndex] + fLvlCount[sIndex]
     
     if fLine[42:55].strip():                # If there's a mixing ratio....
       sStr = "XX="+re.sub(' +',' ',fLine[42:55].strip())
@@ -157,13 +185,11 @@ for fLine in fInput:
         sTrsBE2 = gReturnB(sLine)
       elif sLine[:2] == "M1":
         sTrsBM1 = gReturnB(sLine)
-        
-  fOutput.write(str(nucMass) + " " + str(nucProton) + " " + str(nucMass-nucProton))
 
+        
 fInput.close()
 
-
-
-
+fOutput = open('collate.dat', 'w') # TODO Change to append after testing
+fOutput.write(str(nucMass) + " " + str(nucProton) + " " + str(nucMass-nucProton))
 
 
