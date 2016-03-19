c86f11b0d88fc5499d56b86e864a169bfba49564
Lee Evitts <evitts@triumf.ca>
2016-03-18 21:42:19 -0700

    root drawing

---
+++ createRoot.C
@@ -0,0 +1,170 @@
+#include <TFile.h>
+#include <TTree.h>
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class cNucleus {
+  // Values with uncertainties will be contained in a vector
+  // [0] = value, [1] = value+upper error, [2] = value-lower error
+  vector <Double_t> nMass, nProton, nNeutron, nQuadrupole;
+  vector <Double_t> nDeformation;
+  vector <Double_t> nEnergy0n2, nEnergy2n1, nEnergy4n1, nEnergy2n2;
+  
+  // Transitions
+  vector <Double_t> transID, transLifetime, transBE2, transBM1, transMix, transq2, transRho, transX;
+
+  public:      
+        
+    void SetMass(Double_t sMass) {nMass.push_back(sMass);}
+    void SetProton(Double_t sProton) {nProton.push_back(sProton);}
+    void SetNeutron(Double_t sNeutron) {nNeutron.push_back(sNeutron);}
+    void SetDeformation(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      nDeformation.clear();
+      nDeformation.push_back(sValue);
+      nDeformation.push_back(sUpper);
+      nDeformation.push_back(sLower);
+    }
+    void SetQuadrupole(Double_t sQuad) {nQuadrupole.push_back(sQuad);}
+    
+    
+    void SetEnergy0n2(Double_t sEnergy0n2) {nEnergy0n2.push_back(sEnergy0n2);}
+    void SetEnergy2n1(Double_t sEnergy2n1) {nEnergy2n1.push_back(sEnergy2n1);}
+    void SetEnergy4n1(Double_t sEnergy4n1) {nEnergy4n1.push_back(sEnergy4n1);}
+    void SetEnergy2n2(Double_t sEnergy2n2) {nEnergy2n2.push_back(sEnergy2n2);}
+    
+    void SetID(Int_t sID) {transID.push_back(sID);}
+    
+    
+    void SetLifetime(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      transLifetime.clear();
+      transLifetime.push_back(sValue);
+      transLifetime.push_back(sUpper);
+      transLifetime.push_back(sLower);
+    }
+    void SetBE2(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      transBE2.clear();
+      transBE2.push_back(sValue);
+      transBE2.push_back(sUpper);
+      transBE2.push_back(sLower);
+    }
+    void SetBM1(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      transBM1.clear();
+      transBM1.push_back(sValue);
+      transBM1.push_back(sUpper);
+      transBM1.push_back(sLower);
+    }
+    void SetMix(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      transMix.clear();
+      transMix.push_back(sValue);
+      transMix.push_back(sUpper);
+      transMix.push_back(sLower);
+    }
+    void Setq2(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      transq2.clear();
+      transq2.push_back(sValue);
+      transq2.push_back(sUpper);
+      transq2.push_back(sLower);
+    }
+    void SetRho(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      transRho.clear();
+      transRho.push_back(sValue);
+      transRho.push_back(sUpper);
+      transRho.push_back(sLower);
+    }
+    void SetX(Double_t sValue, Double_t sUpper, Double_t sLower) {
+      transX.clear();
+      transX.push_back(sValue);
+      transX.push_back(sUpper);
+      transX.push_back(sLower);
+    }
+    
+};
+
+
+
+int createRoot() {
+
+  // Variable declaration
+  // .. input file variables
+  int inA, inZ, inN;
+  double inB, inBu, inBl, inQ;
+  double inEnergy0n2, inEnergy2n1, inEnergy4n1, inEnergy2n2;
+  string inTransID
+  double inTransEnergy, inLife, inLifeu, inLifel, inBE2, inBE2u, inBE2l, inBM1, inBM1u, inBM1l;
+  double inMix, inMixu, inMixl, inq2, inq2e, inRho, inRhol, inRhou;
+  double inX, inXe;
+  
+  // Create root file and tree
+  TFile fRootFile("paperExtract.root","RECREATE");
+  TTree *fRootTree = new TTree("T","Nuclear Systematics");
+  // .. and create a new branch for each data member of event object
+  Int_t fSplit = 1; // multi-branch -- what?
+  Int_t fSize = 64000;
+  cNucleus *fNucleus = 0;
+  fRootTree->Branch("Nucleus", "cNucleus", &fNucleus, fSize, fSplit);
+  
+  Int_t fCounter = 1;
+ 
+  // Read in data
+  ifstream inFile("transitions.csv");
+       
+  while (true) {
+    // Read from file
+    inFile >> inA >> inZ >> inN >> inB >> inBu >> inBl >> inQ
+           >> inEnergy0n2 >> inEnergy2n1 >> inEnergy4n1 >> inEnergy2n2
+           >> inTransID >> inTransEnergy
+           >> inLife >> inLifeu >> inLifel >> inBE2 >> inBE2u >> inBE2l
+           >> inBM1 >> inBM1u >> inBM1l >> inMix >> inMixu >>inMixl
+           >> inq2 >> inq2e >> inRho >> inRhol >> inRhou >> inX >> inXe;
+    
+    if (inFile.eof()) break;
+    
+    // Create trans ID
+    Int_t sTransID = 10000;
+    sTransID += inTransC * pow(10, 0);
+    sTransID += inTransD * pow(10, 1);
+    sTransID += inTransA * pow(10, 2);
+    sTransID += inTransB * pow(10, 3);
+    cout << fCounter << "\t" << inA << "\t" << inZ << "\t" << sTransID  << "\t" << inRho << endl;
+        
+
+    fNucleus = new cNucleus;
+     
+      fNucleus->SetMass(inA);
+      fNucleus->SetProton(inZ);
+      fNucleus->SetNeutron(inN);
+             
+      fNucleus->SetDeformation(inB, inBu, inBl);
+      fNucleus->SetQuadrupole(inQ);
+        
+      fNucleus->SetEnergy0n2(inEnergy0n2);
+      fNucleus->SetEnergy2n1(inEnergy2n1);
+      fNucleus->SetEnergy4n1(inEnergy4n1);
+      fNucleus->SetEnergy2n2(inEnergy2n2);
+        
+      fNucleus->SetID(sTransID);
+      fNucleus->SetLifetime( inLife, inLifeu, inLifel);
+      fNucleus->SetBE2( inBE2, inBE2u, inBE2l);
+      fNucleus->SetBM1( inBM1, inBM1u, inBM1l);
+      fNucleus->SetMix(inMix, inMixu, inMixl);
+      fNucleus->Setq2( inq2, inq2e, inq2e );
+      fNucleus->SetRho(inRho, inRhou, inRhol);
+      fNucleus->SetX(inX, inXe, inXe);
+        
+    fCounter++;
+            
+    fRootTree->Fill();
+    delete fNucleus;
+
+  }
+  inFile.close();
+  fRootTree->Write();
+  fRootFile.Close();
+
+  return 0;
+}
---
+++ plotRoot.C
@@ -0,0 +1,103 @@
+#include <TFile.h>
+#include <TString.h>
+#include <TGraphAsymmErrors.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TAxis.h>
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool gDEBUG = true;
+
+/* The options are as follows:
+Nuclear:
+  Mass - nMass, Proton Number - nProton, Neutron Number - nNeutron, Q - nQuadrupole, Beta - nDeformation, 
+  Energy (2nd 0+) - nEnergy0n2, Energy (1st 2+) - nEnergy2n1, Energy (1st 4+) - nEnergy4n1, Energy (2nd 2+) - nEnergy2n2;
+Transitions:
+  Transition ID - transID [A 5 digit number 1 Ji ni Jf nf e.g. the 2nd 2+ to 1st 2+ is 12221],
+  Parent Lifetime [ps] - transLifetime, B(E2) [W.u.] - transBE2, B(M1) [W.u.] - transBM1, 
+  Mixing Ratio - transMix, q^2 - transq2, Rho^2 - transRho, X - transX;
+*/
+string userX = "nDeformation"; // x-axis
+string userY = "transRho"; // y-axis
+string userCondition = "transID"; // condition... bit complicated to implement this
+
+string GetName(string name);
+int plotRoot() {
+
+  // Open file
+  TFile *fFile = TFile::Open("paperExtract.root");
+  TTreeReader fReader("T", fFile);
+  
+  vector <Double_t> fX, fXU, fXL, fY, fYU, fYL;
+   
+  // TTreeReader opens property.  Every property is a vector for convenience, no error values (e.g. Z) have size == 1
+  // For properties with error, the second number is upper error, the third number is lower error
+  TTreeReaderValue< vector<Double_t> > fXValue(fReader, userX.c_str());
+  TTreeReaderValue< vector<Double_t> > fYValue(fReader, userY.c_str());
+  TTreeReaderValue< vector<Double_t> > fCond(fReader, userCondition.c_str());
+  // .. loop through entries in tree, adding to vectors
+  int fCounter = 1;
+  if (gDEBUG) cout << Form("Line\t\t%s\t\t%s\n", GetName(userX).c_str(), GetName(userY).c_str());
+  while (fReader.Next()) {
+    Double_t sX = fXValue->at(0);
+    Double_t sY = fYValue->at(0);
+    if (sX != 0 && sY != 0 && fCond->at(0) == 12221) { //  This is where you would add additional limits e.g. && fCond->at(0) == 12221
+      if (gDEBUG) cout << fCounter << "\t\t" << sX << " " << "\t\t" << sY << endl;
+      fX.push_back(sX);
+      if (fXValue->size() > 1) {
+        fXU.push_back(fXValue->at(1)); fXL.push_back(fXValue->at(2));
+      } else {
+        fXU.push_back(0); fXL.push_back(0);
+      }
+      fY.push_back(sY);
+      if (fYValue->size() > 1) {
+        fYU.push_back(fYValue->at(1)); fYL.push_back(fYValue->at(2));
+      } else {
+        fYU.push_back(0); fYL.push_back(0);
+      }
+    
+    }
+    fCounter++;
+  }
+
+  // Draw Plot
+  Int_t fSize = fX.size();
+  TGraphAsymmErrors *fPlot = new TGraphAsymmErrors(fSize, &fX[0], &fY[0], &fXL[0], &fXU[0], &fYL[0], &fYU[0]);
+  fPlot->SetTitle("");
+  fPlot->GetXaxis()->SetTitle(GetName(userX).c_str());
+  fPlot->GetYaxis()->SetTitle(GetName(userY).c_str());
+  fPlot->SetMarkerStyle(kCircle);
+  fPlot->SetMarkerSize(0.5);
+  fPlot->Draw("AP");
+
+
+  return 0;
+}
+
+string GetName(string fName) {
+
+  if (fName == "nMass") return "Mass Number";
+  if (fName == "nProton") return "Proton Number";
+  if (fName == "nNeutron") return "Neutron Number";
+  if (fName == "nQuadrupole") return "Quadrupole Moment";
+  if (fName == "nDeformation") return "Deformation";
+  if (fName == "nEnergy0n2") return "0^{+}_{2} Energy [keV]";
+  if (fName == "nEnergy2n1") return "2^{+}_{1} Energy [keV]";
+  if (fName == "nEnergy4n1") return "4^{+}_{1} Energy [keV]";
+  if (fName == "nEnergy2n2") return "2^{+}_{2} Energy [keV]";
+  if (fName == "transID") return "Transition ID";
+  if (fName == "transLifetime") return "Parent Lifetime [ps]";
+  if (fName == "transBE2") return "B(E2) [W.u.]";
+  if (fName == "transBM1") return "B(M1) [W.u.]";
+  if (fName == "transMix") return "Mixing Ratio";
+  if (fName == "transq2") return "q^{2}";
+  if (fName == "transRho") return "#rho^{2} (E0) #times 10^{-3}";
+  if (fName == "transX") return "X";
+  else return "UNKNOWN";
+  
+}
